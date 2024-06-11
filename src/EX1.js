03c8a7ac8420ce743712b11e6e010ff3f9fe77b8
matheus kamers <matheus.kamers@checkstore.com.br>
2024-06-11 20:40:09 -0300

    EX1

---
+++ src/EX1.js
@@ -0,0 +1,5 @@
+function filtrarPares(numeros) {
+    return numeros.filter(numero => numero % 2 === 0);
+}
+
+export default filtrarPares;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import filtrarPares from "./EX1.js";
+
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(filtrarPares(numeros)); // [2, 4, 6, 8, 10]
